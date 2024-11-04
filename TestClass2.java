8d2384bea6cf9f785365461e644d0d0d21ac82dc
KCarter336 <168095847+KCarter336@users.noreply.github.com>
2024-11-04 15:18:13 -0500

    Try 2

---
+++ TestClass2.java
@@ -0,0 +1,12 @@
+/* File: TestClass2.java
+ * Author: Keegan Carter
+ * Purpose:
+ */
+
+public class TestClass2 {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+
+}
